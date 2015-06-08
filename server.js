b18992471c4f7864ce3333540d7640a6d5fecc55
AJ Farkas <farkas.aj@gmail.com>
2015-06-08 15:09:14 -0700

    basic server test 2

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http')
 
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'})
   res.write('<h1>Success! Finally!</h1>')
   res.end()
-}).listen(process.env.OPENSHIFT_NODEJS_PORT, process.env.OPENSHIFT_NODEJS_IP)
-console.log('server running on port '+config.port+'.')
+}).listen(port, ip)
+console.log('server running on port '+port+'.')
