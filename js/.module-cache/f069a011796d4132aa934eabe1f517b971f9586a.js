cc110d1175b79b67714fbc3fa41d2ffc59722d13
AJ Farkas <farkas.aj@gmail.com>
2015-06-16 08:56:26 -0400

    rm module cache

--- js/.module-cache/f069a011796d4132aa934eabe1f517b971f9586a.js
+++
@@ -1,9 +0,0 @@
-var CommentList = react.createClass({
-  render: function() {
-    return (
-      React.createElement("div", {className: "comment-list"}, 
-        React.createElement("p", null, "I am Comment List!")
-      )
-    )
-  }
-})
